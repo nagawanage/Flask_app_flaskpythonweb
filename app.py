25cf5c31d5690824518d9c889825d54d7285605e
wataru_nagatsuka <wataru.nagatsuka@bric.jp>
2021-08-28 22:26:37 +0900

    add: templateの関数インポート、セッション

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from typing import ClassVar
-from flask import Flask, render_template, redirect
-from flask.globals import request
-from wtforms import StringField, SubmitField, IntegerField
-from wtforms.form import Form
-import os
+from flask import Flask, render_template, redirect, request, session, url_for
+from wtforms import Form
+from wtforms import (
+    StringField, SubmitField, IntegerField, BooleanField, DateField,
+    PasswordField, RadioField, SelectField, TextAreaField
+)
 
 
 app = Flask(__name__)
@@ -13,28 +13,40 @@ app.config['SECRRET_KEY'] = b'\xd8m\x1d\xd2\x0f\xc8\xf2\x1b^R\x8d1\xd6>\x0c\xb0'
 
 
 class UserForm(Form):
-    name = StringField('名前')
-    age = IntegerField('年齢')
+    name = StringField('名前：')
+    messag = IntegerField('年齢：')
+    password = PasswordField('パスワード：')
+    birthday = DateField('誕生日：', format='%Y/%m/%d')
+    gender = RadioField(
+        '性別：', choices=[('man', '男性'), ('woman', '女性')])
+    major = SelectField('専攻：', choices=[
+        ('bungaku', '文学部'), ('hougaku', '法学部'), ('rigaku', '理学部')])
+
+    is_japanese = BooleanField('日本人？')
+    message = TextAreaField('message：')
     submit = SubmitField('Submit')
 
 
-@app.route('/', methods=['GET', 'POST'])
+@ app.route('/', methods=['GET', 'POST'])
 def index():
-    name = age = ''
     form = UserForm(request.form)
+    if request.method == "POST" and form.validate():
+        session['name'] = form.name.data
+        session['age'] = form.age.data
+        session['birthday'] = form.birthday.data
+        session['gender'] = form.gender.data
+        session['major'] = form.major.data
+        session['nationality'] = '日本人' if
+        form.is_japanese.data else '外国人'
+        session['message'] = form.message.data
+        return redirect(url_for('show_user'))
 
-    if request.method == 'POST':
-        # ユーザーの入力内容
-        # 型チェック
-        if form.validate():
-            # 取得
-            name = form.name.data
-            age = form.age.data
-            form = UserForm()
-        else:
-            print('入力内容に誤りがあります')
+    return render_template('user_regist.html', form=form)
 
-    return render_template('index.html', form=form, name=name, age=age)
+
+@ app.route('/show_user')
+def show_user():
+    return render_template('show_user.html')
 
 
 if __name__ == "__main__":
