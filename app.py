67ed64715349f1382f06730944d4f426bd88400e
wataru_nagatsuka <wataru.nagatsuka@bric.jp>
2021-08-29 21:06:46 +0900

    add: Flask(バリデーション)

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, redirect, request, session, url_for
+from flask import Flask, render_template, redirect, request, session, url_for, flash
 from wtforms import Form
 from wtforms import (
     StringField, SubmitField, IntegerField, BooleanField, DateField,
     PasswordField, RadioField, SelectField, TextAreaField
 )
 from wtforms.widgets import TextArea
-
+from wtforms.validators import (
+    DataRequired, EqualTo, Length, NumberRange, ValidationError
+)
+from datetime import date
 
 app = Flask(__name__)
 
@@ -13,21 +16,56 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = b'\xd8m\x1d\xd2\x0f\xc8\xf2\x1b^R\x8d1\xd6>\x0c\xb0'
 
 
+def validate_name2(form, field):
+    if field.data == 'fuga':
+        raise ValidationError('その名前は使用できません(validate_name2)')
+
+
 class UserForm(Form):
     # name = StringField('名前：', default='田中　太郎')
-    name = StringField('名前：', widget=TextArea(), default='田中　太郎')
-    age = IntegerField('年齢：')
-    password = PasswordField('パスワード：')
+    name = StringField('名前：',
+                       # validators=[DataRequired('名前を入力してください')],
+                       validators=[validate_name2,
+                                   DataRequired('名前を入力してください')],
+                       default='田中　太郎')
+    age = IntegerField('年齢：',
+                       validators=[NumberRange(0, 100, '誤りがあります')])
+    password = PasswordField(
+        'パスワード：',
+        validators=[Length(1, 10, '10文字以内にしてください'),
+                    EqualTo('confirm_password', 'パスワードが一致しません')]
+    )
+    confirm_password = PasswordField('パスワード確認用：')
     birthday = DateField('誕生日：', format='%Y/%m/%d',
                          render_kw={"placeholder": "yyyy/mm/dd"})
     gender = RadioField(
         '性別：', choices=[('man', '男性'), ('woman', '女性')], default='man')
     major = SelectField('専攻：', choices=[
         ('bungaku', '文学部'), ('hougaku', '法学部'), ('rigaku', '理学部')])
-
     is_japanese = BooleanField('日本人？')
     message = TextAreaField('message：')
     submit = SubmitField('Submit')
+
+    # def validate_<対象のfield名> でそのfieldに対するvalidation
+    def validate_name(self, field):
+        """"nameのvalidation"""
+        if field.data == 'hoge':
+            raise ValidationError('その名前は使用できません')
+
+    def validate(self):
+        # 既存のvalidation
+        if not super(UserForm, self).validate():
+            return False
+
+        today = date.today()
+        birthday = self.birthday.data
+        birthday_of_this_age = birthday.replace(
+            year=birthday.year + self.age.data)
+        if 0 <= (today - birthday_of_this_age).days <= 365:
+            return True
+
+        flash('年齢と誕生日の関係に誤りがあります')
+        return False
 
 
 @app.route('/', methods=['GET', 'POST'])
